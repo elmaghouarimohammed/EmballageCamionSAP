5df4372d0395b71951a479ea2b8b9958f710b767
mohammed0998 <majidchak2@gmail.com>
2026-09-16 19:24:30 +0100

    data

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -7,3 +7,6 @@ Route::get('/calculations', [CalculationController::class, 'index']);
 Route::post('/calculations', [CalculationController::class, 'store']);
 Route::get('/calculations/{calculation}', [CalculationController::class, 'show']);
 Route::delete('/calculations/{calculation}', [CalculationController::class, 'destroy']);
+Route::get('/calculations', function () {
+    return \App\Models\Calculation::all();
+});
