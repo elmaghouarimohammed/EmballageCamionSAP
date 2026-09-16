dfe5705329e3db08fcc74867b182eff587bdea55
mohammed0998 <majidchak2@gmail.com>
2026-09-16 18:31:12 +0100

    Update api2

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -45,10 +45,5 @@ const API = axios.create({
   baseURL: import.meta.env.VITE_API_URL + '/api',
 });
 
-import axios from 'axios';
 
-const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL + '/api',
-});
 
-export default API;
