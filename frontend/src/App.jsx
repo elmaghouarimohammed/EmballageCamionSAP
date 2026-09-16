78f613ed8c4bf5f17fa7ecbeefbe5caa7898fde5
mohammed0998 <majidchak2@gmail.com>
2026-09-16 19:41:34 +0100

    data2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,29 +3,8 @@ import Layout from './components/Layout';
 import Difference from './pages/Difference';
 import Home from './pages/Home';
 import Launchpad from './pages/Launchpad';
-import { useEffect, useState } from 'react';
-import API from './api';
 
 export default function App() {
-  const [data, setData] = useState([]);
-  useEffect(() => {
-    API.get('/test-route') // حط هنا الـ Route اللي درتي فـ routes/api.php فـ Laravel
-      .then(response => {
-        console.log('Data from Laravel:', response.data);
-        setData(response.data);
-      })
-      .catch(error => {
-        console.error('Error fetching data:', error);
-      });
-  }, []);
-
-  return (
-    <div>
-      <h1>الاتصال مع Laravel</h1>
-      <pre>{JSON.stringify(data, null, 2)}</pre>
-    </div>
-  );
-  
   return (
     <BrowserRouter>
       <Routes>
